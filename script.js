02a6c4b10289dc1e1b2bae8b9216bd4e93feead1
Kumail Khan <liamukkhan@gmail.com>
2024-08-21 18:46:07 +0500

    javascrpt

---
+++ script.js
@@ -0,0 +1,36 @@
+function sidebarMenu() {
+    let sidebar = document.querySelector(".sidebar-menu")
+    let menu = document.querySelector(".menu")
+    let cancel = document.querySelector(".cancel")
+
+    // sidebar.style.marginLeft = "560px"
+    // console.log(sidebar)
+
+    if (sidebar.style.left === "-560px" || sidebar.style.marginLeft === "") {
+        console.log(sidebar.style.left)
+        sidebar.style.left = "0px"
+        cancel.style.display = "block"
+        menu.style.display = "none"
+    }
+    else {
+        console.log(sidebar.style.left)
+        sidebar.style.left = "-560px"
+        cancel.style.display = "none"
+        menu.style.display = "block"
+    }
+
+
+}
+
+
+
+
+// let menu = document.querySelector(".menu")
+// let cancel = document.querySelector(".cancel")
+// let sidebarMenu = document.querySelector(".sidebar-menu")
+
+// sidebarMenu
+
+// menu.addEventListener("click", () => {
+
+// })
