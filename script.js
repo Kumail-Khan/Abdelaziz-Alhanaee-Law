fcf2240663fbda27eb050ceb2e7f3e5cb11a98c0
Kumail Khan <liamukkhan@gmail.com>
2024-08-21 22:57:12 +0500

    search button not fix

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
 function sidebarMenu() {
     let sidebar = document.querySelector(".sidebar-menu")
-    let menu = document.querySelector(".menu")
-    let cancel = document.querySelector(".cancel")
+    let menuBtn = document.querySelector(".menu")
+    let cancelBtn = document.querySelector(".cancel")
 
-    // sidebar.style.marginLeft = "560px"
-    // console.log(sidebar)
+
 
     if (sidebar.style.left === "-560px" || sidebar.style.left === "") {
-        console.log(sidebar.style.left)
         sidebar.style.left = "0px"
-        cancel.style.display = "block"
-        menu.style.display = "none"
-    }
-    else {
-        console.log(sidebar.style.left)
+        cancelBtn.style.display = "block"
+        menuBtn.style.display = "none"
+    } else {
         sidebar.style.left = "-560px"
-        cancel.style.display = "none"
-        menu.style.display = "block"
+        cancelBtn.style.display = "none"
+        menuBtn.style.display = "block"
     }
 
 
 }
 
+function searchBoxFunction() {
+    let cancelBtn = document.querySelector(".cancel-2")
+    let searchBtn = document.querySelector(".search")
+    let searchBox = document.querySelector(".search-box")
 
-// let menu = document.querySelector(".menu")
-// let cancel = document.querySelector(".cancel")
-// let sidebarMenu = document.querySelector(".sidebar-menu")
+    console.log(searchBox.style.right)
 
-// sidebarMenu
+    if (searchBox.style.right === "1820px" || searchBox.style.right === "") {
+        searchBox.style.left = "93px"
+        cancelBtn.style.display = "block"
+        searchBtn.style.display = "none"
+    } else {
+        searchBox.style.right = "1820px"
+        cancelBtn.style.display = "none"
+        searchBtn.style.display = "block"
+    }
 
-// menu.addEventListener("click", () => {
 
-// })
+}
 
-// let menu = document.querySelector(".menu")
-// let cancel = document.querySelector(".cancel")
-// let sidebarMenu = document.querySelector(".sidebar-menu")
-
-// sidebarMenu
-
-// menu.addEventListener("click", () => {
-
-// })
